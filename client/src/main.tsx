282cfe2cf5585f122dd636de605a975e5ae1d0ae
vodkashot <183611920+Vodkashot28@users.noreply.github.com>
2025-11-18 18:08:32 +0800

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,12 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const manifestUrl = "https://solar-system.xyz/tonconnect-manifest.json";
+
+createRoot(document.getElementById("root")!).render(
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
+    <App />
+  </TonConnectUIProvider>
+);
